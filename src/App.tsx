956c61d50c59b923c2a7c89ef5a18e72c15d2a09
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-15 20:39:16 -0300

    create api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
-import { GlobalStyled } from './styles/global';
+import React from "react";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyled } from "./styles/global";
+import { createServer } from "miragejs";
 
+createServer({
+  routes() {
+    this.namespace = "api";
+  },
+});
 
 export function App() {
   return (
     <>
-        <Header/>
-        <Dashboard/>
-        <GlobalStyled/>
-   </>
+      <Header />
+      <Dashboard />
+      <GlobalStyled />
+    </>
   );
 }
-
