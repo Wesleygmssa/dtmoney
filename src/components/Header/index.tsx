47b66ffa211e07347620f5483930b8a7c237757b
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-15 21:17:14 -0300

    CREATE MODAL

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import logoImg from "../../assets/logo.svg";
 import { Conatiner, Content } from "./styels";
+import Modal from "react-modal";
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <Conatiner>
       <Content>
         <img src={logoImg} alt="dt many" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>
+          Nova transação
+        </button>
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       </Content>
     </Conatiner>
   );
